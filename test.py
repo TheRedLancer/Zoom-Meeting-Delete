ab96885ae63b23fbbd0bf9d34e112ab7230143e7
Zachary Burnaby <zachary.burnaby@kennedyhs.org>
2020-09-10 11:21:41 -0700

    Work through a python API tutorial

--- test.py
+++ test.py
@@ -1,14 +1,51 @@
 import requests
-import pyjwt
+import json
+import jwt
 
-# Search GitHub's repositories for requests
-response = requests.get(
-    'https://api.github.com/search/repositories',
-    params={'q': 'requests+language:python'},
-)
+from datetime import datetime
 
-# Inspect some attributes of the `requests` repository
-json_response = response.json()
-repository = json_response['items'][0]
-print(f'Repository name: {repository["name"]}')  # Python 3.6+
-print(f'Repository description: {repository["description"]}')  # Python 3.6+
+def jprint(obj):
+    # Create and print string of JSON Object
+    # Grabbed from "https://www.dataquest.io/blog/python-api-tutorial/"
+    text = json.dumps(obj, sort_keys=True, indent = 4)
+    print(text)
+
+ROOT = "http://api.open-notify.org" # Root url for the api
+
+endpoint = '/iss-pass.json'
+
+headers = {
+    'authorization': "PUT STUFF HERE", #YOU NEED THIS
+    'content-type': "application/json"
+}
+
+perameters = {
+    "lat": 40.71,
+    "lon": -74
+}
+
+res = requests.get(ROOT + endpoint, perameters)
+
+jprint(res.json())
+
+
+"""
+pass_times = res.json()['response']
+print("Pass Times: ")
+jprint(pass_times)
+
+risetimes = []
+
+for d in pass_times:
+    time = d['risetime']
+    risetimes.append(time)
+
+print(risetimes)
+
+times = []
+
+for rt in risetimes:
+    time = datetime.fromtimestamp(rt)
+    times.append(time)
+    print(time)
+"""
