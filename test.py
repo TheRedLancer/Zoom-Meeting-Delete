44533b73f8e7e003f78403cb67ca1df55b96dcab
TheRedLancer <zburnaby918@gmail.com>
2020-09-11 13:15:17 -0700

    test

--- test.py
+++ test.py
@@ -4,18 +4,22 @@ import jwt
 from time import time
 import secrets
 
+
 def jprint(obj):
     # Create and print string of JSON Object
     # Grabbed from "https://www.dataquest.io/blog/python-api-tutorial/"
-    text = json.dumps(obj, sort_keys=True, indent = 4)
+    text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-API_KEY = "API KEY HERE"
 
-API_SECRET = "API SECRET HERE"
+API_KEY = "pP13W03hT7CLEKpVZqZBLw"
+
+API_SECRET = "6Rnhhgr9efz50Julz2HhIZ0z90XCftfuqhkm"
 
 # create a function to generate a token using the pyjwt library
-# Copied from https://devforum.zoom.us/t/zoom-jwt-token-creation-automate-the-process/17708 Code by user michael.harrington
+# Grabbed from https://devforum.zoom.us/t/zoom-jwt-token-creation-automate-the-process/17708 Code by user michael.harrington
+
+
 def generateToken():
     token = jwt.encode(
         # Create a payload of the token containing API Key & expiration time
@@ -29,13 +33,15 @@ def generateToken():
 
     return token
 
-def getUsers(): 
+
+def getUsers():
     headers = {'authorization': 'Bearer %s' % generateToken(),
                'content-type': 'application/json'}
 
     r = requests.get('https://api.zoom.us/v2/users/', headers=headers)
 
     return(r)
+
 
 def getMeetings():
     headers = {'authorization': 'Bearer %s' % generateToken(),
@@ -45,4 +51,5 @@ def getMeetings():
 
     return(r)
 
-jprint(getMeetings().json())
+
+jprint(getUsers().json())
