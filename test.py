c59d597cbcc69a4c0c5a9a545135e03ad47c95ac
TheRedLancer <zburnaby918@gmail.com>
2020-09-14 21:22:21 -0700

    clean up test.py

--- test.py
+++ test.py
@@ -15,53 +15,58 @@ with open("APIKey.jwt", "r") as jwtFile:
 API_KEY = JWT[0]
 API_SECRET = JWT[1]
 
-meetingsToDelete = []
-
 startDate = "2020-08-20"
 endDate = "2020-08-26"
+
+meetingsToDelete = []
 
 # Make first call
 r = API.getAccountRecordings(
     API_KEY, API_SECRET, 'me', startDate, endDate, "")
+
 # Convert response to JSON
 apiRecording = r.json()
+
 # Print returned call
 # jprint(apiRecording)
+
 # Set next page token variable
 next_page_token = apiRecording['next_page_token']
+
 # Don't delete kchszoom recordings
 if apiRecording["meetings"][0]["host_email"] != "kchszoom@kennedyhs.org":
     # Append Meeting ID to meetingsToDelete
     meetingsToDelete.append(apiRecording["meetings"][0]["uuid"])
-# Print Meeting ID
+
+# Print Meeting Data
 print(apiRecording["meetings"][0]["uuid"], r,
       apiRecording["meetings"][0]["host_email"],
       apiRecording["meetings"][0]["topic"])
-# Line Break
-# print("-------------------------------------------------")
 
 while next_page_token != '':
     # Make sequential call with next page token from prev call
     apiRecording = API.getAccountRecordings(
         API_KEY, API_SECRET, 'me', startDate, endDate, next_page_token).json()
-    # Print returned call
+
+    # Print returned call (debugging)
     # jprint(apiRecording)
+
     # Set next page token variable
     next_page_token = apiRecording['next_page_token']
+
     # If the name of the user is not the school's account, append the MeetingID to the delete queue
     if apiRecording["meetings"][0]["host_email"] != "kchszoom@kennedyhs.org":
         # Append Meeting ID to meetingsToDelete
         meetingsToDelete.append(apiRecording["meetings"][0]["uuid"])
-    # Print MeetingID
+
+    # Print Meeting Data
     print(apiRecording["meetings"][0]["uuid"], r,
           apiRecording["meetings"][0]["host_email"],
           apiRecording["meetings"][0]["topic"])
-    # Line Break
-    # print("-------------------------------------------------")
 
 # print(meetingsToDelete)
+
 print("Number of meetings to delete:", len(meetingsToDelete))
-# Delete all meetings in the meetingsToDelete array
 
 
 def deleteMeetings(API_KEY, API_SECRET, meetingsToDelete):
@@ -72,6 +77,7 @@ def deleteMeetings(API_KEY, API_SECRET, meetingsToDelete):
         print(meeting, n)
 
 
+# Delete all meetings in the meetingsToDelete array
 deleteMeetings(API_KEY, API_SECRET, meetingsToDelete)
 
 
